c207b9cef8ba9e0a9dd65fdd02c1764f1f38e20b
Nishant Thapa <itsnishantu@gmail.com>
2024-07-17 13:19:22 +0545

    adding the start button and quiz questions

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
       home: QuizHomePage(),
     );
   }
@@ -35,18 +38,30 @@ class _QuizHomePageState extends State<QuizHomePage> {
   int _currentQuestionIndex = 0;
   int _score = 0;
   bool _quizFinished = false;
+  bool _quizStarted = false;
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _loadQuestions();
   }
 
   Future<void> _loadQuestions() async {
+    setState(() {
+      _isLoading = true;
+    });
     List<dynamic> questions = await loadJson();
     setState(() {
       _questionsList = questions;
+      _isLoading = false;
     });
+  }
+
+  void _startQuiz() {
+    setState(() {
+      _quizStarted = true;
+    });
+    _loadQuestions();
   }
 
   void _answerQuestion(String answer) {
@@ -67,6 +82,7 @@ class _QuizHomePageState extends State<QuizHomePage> {
       _currentQuestionIndex = 0;
       _score = 0;
       _quizFinished = false;
+      _quizStarted = false;
     });
   }
 
@@ -75,6 +91,13 @@ class _QuizHomePageState extends State<QuizHomePage> {
       onPressed: () {
         _answerQuestion(option);
       },
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+        textStyle: TextStyle(fontSize: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
       child: Text(option),
     );
   }
@@ -90,48 +113,74 @@ class _QuizHomePageState extends State<QuizHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    if (_quizFinished) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Quiz App"),
-          centerTitle: true,
-          backgroundColor: Colors.yellow,
-          elevation: 10,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text('Your Score: $_score / ${_questionsList!.length}'),
-              SizedBox(height: 10),
-              ElevatedButton(
-                  onPressed: _restartQuiz, child: Text('Restart Quiz')),
-            ],
-          ),
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Quiz App"),
-          centerTitle: true,
-          backgroundColor: Colors.yellow,
-          elevation: 10,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                _questionsList![_currentQuestionIndex]['question'],
-                style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz App"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: _quizStarted
+            ? _isLoading
+                ? CircularProgressIndicator()
+                : _quizFinished
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Your Score: $_score / ${_questionsList!.length}',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: _restartQuiz,
+                            child: Text('Restart Quiz'),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 15.0, horizontal: 30.0),
+                              textStyle: TextStyle(fontSize: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            _questionsList![_currentQuestionIndex]['question'],
+                            style: TextStyle(fontSize: 20),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 20),
+                          ..._buildOptions(),
+                        ],
+                      )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome to the Quiz App!',
+                    style: TextStyle(fontSize: 24),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _startQuiz,
+                    child: Text('Start Quiz'),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 30.0),
+                      textStyle: TextStyle(fontSize: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 10,
-              ),
-              ..._buildOptions(),
-            ],
-          ),
-        ),
-      );
-    }
+      ),
+    );
   }
 }
